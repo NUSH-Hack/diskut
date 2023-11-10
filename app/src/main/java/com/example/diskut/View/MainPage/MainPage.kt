735a21db8b19c197196afe7db2e8c75fc4bf0b37
Viron <josephziegler19@gmail.com>
2023-11-11 06:34:06 +0800

    Fixed error with placing code with vital timing outside coroutine

--- app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
+++ app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
@@ -1,13 +1,27 @@
 package com.example.diskut.View.MainPage
 
+import android.content.res.Configuration
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -16,12 +30,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.diskut.Bluetooth
 import com.example.diskut.Controller.GenerateConversationCues
-import com.example.diskut.Controller.PeerConnect
+import com.example.diskut.MainActivity
 import com.example.diskut.Model.User
 import com.example.diskut.Model.UserType
+import com.example.diskut.ui.theme.AppTheme
 
 val yueheng = User("h2010157@nushigh.edu.sg", "Wong Yue Heng", UserType.STUDENT, "Year 4", 0)
 val warren = User("h2230006@nushigh.edu.sg", "Warren Zhou", UserType.STUDENT, "Year 4", 10)
@@ -47,11 +65,37 @@ fun MainPage(goalPoints: Int, bluetooth: Bluetooth) {
         modifier = Modifier
             .fillMaxSize()
             .clickable(interactionSource = interactionSource, indication = null) {
-                peerUser = PeerConnect(currUser, bluetooth) {
-                    conversationCues.clear()
-                    conversationCues.addAll(GenerateConversationCues(it, currUser))
+                val peer = ByteArray(1024)
+
+                if (bluetooth.adapter.name == "Jing's A12") {
+                    Log.i("diskut", "warren")
+                    bluetooth.startServer {
+                        Log.i("diskut", "connection established")
+                        bluetooth.send(currUser.serialize())
+                        bluetooth.receive(peer)
+
+                        peerUser = User.deserialize(peer)
+
+                        expanded = !expanded
+
+                        conversationCues.clear()
+                        conversationCues.addAll(GenerateConversationCues(peerUser, currUser))
+                    }
+                } else {
+                    Log.i("diskut", "not warren")
+                    bluetooth.startClient {
+                        Log.i("diskut", "connection established")
+                        bluetooth.send(currUser.serialize())
+                        bluetooth.receive(peer)
+
+                        peerUser = User.deserialize(peer)
+
+                        expanded = !expanded
+
+                        conversationCues.clear()
+                        conversationCues.addAll(GenerateConversationCues(peerUser, currUser))
+                    }
                 }
-                expanded = !expanded
             }) {
 
         Column(
