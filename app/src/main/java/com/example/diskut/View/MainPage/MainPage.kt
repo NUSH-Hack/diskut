895631ef839b9b4b32fc18465a148fa78f97be37
Viron <josephziegler19@gmail.com>
2023-11-11 04:58:37 +0800

    Added an expanded and unexpanded state for the MainPage Added an animation that can transition between the two states

--- app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
+++ app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
@@ -1,12 +1,26 @@
 package com.example.diskut.View.MainPage
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -21,18 +36,49 @@ import com.example.diskut.ui.theme.AppTheme
 
 @Composable
 fun MainPage(goalPoints: Int) {
+    var expanded by remember { mutableStateOf(true) }
     var currPoints by remember { mutableStateOf(270) }
 
-    Column(
-        modifier = Modifier.fillMaxSize().padding(24.dp),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top
-    ) {
-        PointsIndicator(username = "Larry", currPoints = currPoints, goalPoints = goalPoints)
+    val interactionSource = remember { MutableInteractionSource() }
 
-        ConversationCues(modifier = Modifier.weight(1.0f), text="You are both stupid")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(interactionSource = interactionSource, indication = null) {
+                expanded = !expanded
+            }) {
+        
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            AnimatedPointsIndicator(
+                modifier = Modifier.weight(0.1f),
+                username = "Larry",
+                currPoints = currPoints,
+                goalPoints = goalPoints,
+                expanded = expanded
+            )
 
-        PeerDetails(username = "Jerry", occupation="Student - M23404")
+            AnimatedConversationCues(
+                modifier = Modifier.weight(10.0f),
+                text = "You are both stupid",
+                expanded = expanded
+            )
+
+            Spacer(modifier = Modifier.weight(0.1f))
+
+            AnimatedPeerDetails(
+                modifier = Modifier.weight(0.1f),
+                username = "Jerry",
+                occupation = "Student - M23404",
+                expanded = expanded
+            )
+
+        }
     }
 }
 
--- app/src/main/java/com/example/diskut/View/MainPage/PointsIndicator.kt
+++ app/src/main/java/com/example/diskut/View/MainPage/PointsIndicator.kt
@@ -1,16 +1,61 @@
 package com.example.diskut.View.MainPage
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import kotlin.math.exp
+
+@Composable
+internal fun AnimatedPointsIndicator(
+    modifier: Modifier = Modifier,
+    username: String,
+    currPoints: Int,
+    goalPoints: Int,
+    expanded: Boolean
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = if (expanded) Arrangement.Top else Arrangement.Center,
+        horizontalAlignment = if (expanded) Alignment.Start else Alignment.CenterHorizontally
+    ) {
+        
+        Text(
+            modifier = Modifier.animateContentSize(),
+            text = username,
+            style = if (expanded) MaterialTheme.typography.titleMedium else MaterialTheme.typography.displaySmall,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start
+        )
+
+        Text(
+            modifier = Modifier.animateContentSize(),
+            text = "$currPoints/$goalPoints",
+            style = if (expanded) MaterialTheme.typography.labelSmall else MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Start
+        )
+
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(16.dp),
+            progress = currPoints.toFloat() / goalPoints.toFloat(),
+        )
+
+    }
+}
 
 @Composable
 internal fun PointsIndicator(
