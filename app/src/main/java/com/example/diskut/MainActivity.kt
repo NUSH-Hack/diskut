a710bddcd1722f13736b16cf7645415f32af7c4a
azazazo <h2010138@nushigh.edu.sg>
2023-11-10 19:36:03 +0800

    bluetooth is driving me insane

--- app/src/main/java/com/example/diskut/MainActivity.kt
+++ app/src/main/java/com/example/diskut/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.diskut
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothServerSocket
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +22,60 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.diskut.ui.theme.DiskutTheme
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private lateinit var socket: BluetoothServerSocket
+    private val uuid = UUID.randomUUID()
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action!!
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice =
+                        intent.getParcelableExtra(
+                            BluetoothDevice.EXTRA_DEVICE
+                        )!!
+                    val deviceName = device.name
+                    val deviceMAC = device.address
+                    Log.i("diskut", "Found device: $deviceName ($deviceMAC)")
+
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i("diskut", "requesting perms")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                ),
+                1337
+            )
+        }
+
         setContent {
             DiskutTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,9 +83,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(":3")
                 }
             }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        unregisterReceiver(receiver)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i("diskut", "meowow $requestCode")
+        when (requestCode) {
+            1337 -> {
+                Log.i("diskut", "perms request accepted")
+                registerReceiver(receiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
+                Log.i("diskut", "start: ${bluetoothAdapter.startDiscovery()}")
+            }
+            else -> {}
         }
     }
 }
