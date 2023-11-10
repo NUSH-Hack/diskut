0b2a664408d12625bea0a416ace79c07e7b7c416
Viron <josephziegler19@gmail.com>
2023-11-10 20:36:57 +0800

    Started creating QuestPage Renamed theme package for consistency

--- app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
+++ app/src/main/java/com/example/diskut/View/MainPage/MainPage.kt
@@ -3,34 +3,24 @@ package com.example.diskut.View.MainPage
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose.AppTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.example.diskut.ui.theme.AppTheme
 
 @Composable
 fun MainPage(goalPoints: Int, content_padding: Dp) {
-
     var currPoints by remember { mutableStateOf(270) }
 
     Column(
---
+++ app/src/main/java/com/example/diskut/View/QuestPage/QuestPage.kt
@@ -0,0 +1,51 @@
+package com.example.diskut.View.QuestPage
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.diskut.ui.theme.AppTheme
+import androidx.compose.foundation.lazy.items
+
+@Composable
+fun QuestPage(content_padding: Dp) {
+    val questList = remember { mutableStateListOf<Quest>() }
+    questList.addAll(q)
+
+    LazyColumn(modifier = Modifier
+        .fillMaxSize()
+        .padding(content_padding), state = rememberLazyListState()) {
+        items(questList, itemContent = { item ->
+            QuestCard(item)
+        })
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun MainPagePreview() {
+    AppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            QuestPage(32.dp)
+        }
+    }
+}
+
+
+
+internal val q: List<Quest> = listOf<Quest>(
+
+)
