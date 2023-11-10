bf7053d0b92659edb8108262604524a272e22c48
Viron <josephziegler19@gmail.com>
2023-11-10 21:23:26 +0800

    Optimized imports in all files Finished quest card ui

--- app/src/main/java/com/example/diskut/View/QuestPage/QuestCard.kt
+++ app/src/main/java/com/example/diskut/View/QuestPage/QuestCard.kt
@@ -1,25 +1,50 @@
 package com.example.diskut.View.QuestPage
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.diskut.View.MainPage.MainPage
 import com.example.diskut.ui.theme.AppTheme
 
 @Composable
 internal fun QuestCard(quest: Quest, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Row {
-            Text(text = quest.description)
-            Text(text = "${quest.completed}/${quest.goal}")
+    Card(modifier = modifier
+        .fillMaxWidth()
+        .height(80.dp)) {
+        Row(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                modifier = Modifier.weight(1f),
+                text = quest.description,
+                style = MaterialTheme.typography.labelLarge,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = "${quest.completed}/${quest.goal}",
+                style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1
+            )
         }
     }
 }
@@ -28,11 +53,12 @@ internal fun QuestCard(quest: Quest, modifier: Modifier = Modifier) {
 @Composable
 private fun QuestCardPreview() {
     AppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            MainPage(1000, 32.dp)
-        }
+        QuestCard(
+            Quest(
+                description = "Talk to 100 people",
+                completed = 1,
+                goal = 100
+            )
+        )
     }
 }
--- app/src/main/java/com/example/diskut/View/QuestPage/QuestPage.kt
+++ app/src/main/java/com/example/diskut/View/QuestPage/QuestPage.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,7 +16,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.diskut.ui.theme.AppTheme
-import androidx.compose.foundation.lazy.items
 
 @Composable
 fun QuestPage(content_padding: Dp) {
