dea86a562f26d889e3ccb02d0698aa3cdeb1bbc6
azazazo <h2010138@nushigh.edu.sg>
2023-11-10 20:02:01 +0800

    added support for discovering bluetooth devices/making device discoverable also ui added to make it easier to test

--- app/src/main/java/com/example/diskut/MainActivity.kt
+++ app/src/main/java/com/example/diskut/MainActivity.kt
@@ -15,13 +15,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.example.diskut.ui.theme.DiskutTheme
 import java.util.UUID
@@ -38,7 +44,7 @@ class MainActivity : ComponentActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val action: String = intent.action!!
-            when(action) {
+            when (action) {
                 BluetoothDevice.ACTION_FOUND -> {
                     // Discovery has found a device. Get the BluetoothDevice
                     // object and its info from the Intent.
@@ -83,7 +89,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(":3")
+                    Greeting(
+                        ":3",
+                        discoverer = this::startDiscover,
+                        discoveree = this::makeDiscoverable,
+                        endDiscover = this::endDiscover,
+                    )
                 }
             }
         }
@@ -106,25 +117,78 @@ class MainActivity : ComponentActivity() {
             1337 -> {
                 Log.i("diskut", "perms request accepted")
                 registerReceiver(receiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
-                Log.i("diskut", "start: ${bluetoothAdapter.startDiscovery()}")
             }
+
             else -> {}
+        }
+    }
+
+    fun startDiscover() {
+        if (bluetoothAdapter.isDiscovering) endDiscover()
+        bluetoothAdapter.startDiscovery()
+    }
+
+    fun endDiscover() {
+        bluetoothAdapter.cancelDiscovery()
+    }
+
+    fun makeDiscoverable() {
+        val requestCode = 1;
+        val discoverableIntent: Intent =
+            Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            }
+        startActivityForResult(discoverableIntent, requestCode)
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+    discoverer: () -> Unit,
+    discoveree: () -> Unit,
+    endDiscover: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Row {
+                Button(
+                    onClick = discoverer
+                ) {
+                    Text("start discovery")
+                }
+            }
+            Row {
+                Button(
+                    onClick = endDiscover
+                ) {
+                    Text("end discovery")
+                }
+            }
+        }
+        Button(
+            onClick = discoveree
+        ) {
+            Text("be discovered")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiskutTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DiskutTheme {
+//        Greeting("Android")
+//    }
+//}
